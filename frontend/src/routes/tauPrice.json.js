de803ab8a2d7bcb39a1a87b9e5b82dd93557de0a
kels7 <hiketabo@gmail.com>
2024-09-06 09:10:38 +0000

    chore: change currency symbol

--- frontend/src/routes/tauPrice.json.js
+++ frontend/src/routes/tauPrice.json.js
@@ -1,6 +1,6 @@
 export async function get(req, res) {
 
-    let priceData = await global.db.models.Prices.findOne({symbol: 'TAU'})
+    let priceData = await global.db.models.Prices.findOne({symbol: 'XIAN'})
     if (!priceData) priceData = { currentPrice: 0}
 
     res.setHeader('Content-Type', 'application/json');
